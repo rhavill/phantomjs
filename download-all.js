7b1687d179523bd607e98953bc717dbe863d1c2d
Ricardo Havill <rhavill@progresstesting.com>
2014-04-07 14:12:23 -0400

    Added config file.

---
+++ download-all.js
@@ -0,0 +1,7 @@
+var webPage = require('webpage');
+var fs = require('fs');
+var page = webPage.create();
+var config = require('./download-all-config');
+
+console.log(config.baseUrl);
+phantom.exit();
